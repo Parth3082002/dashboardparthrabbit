578d3b9bdef812305eca8372fbabd9bd3859fcf8
Parth3082002 <parth3082002@gmail.com>
2026-05-09 20:39:02 +0530

    live backend api

--- src/services/api.js
+++ src/services/api.js
@@ -8,8 +8,8 @@
 import axios from "axios";
 
 const API = axios.create({
-   baseURL: "http://localhost:5000/api",
-  // baseURL: "https://parthdemorabitmq.onrender.com/api",
+  //  baseURL: "http://localhost:5000/api",
+  baseURL: "https://parthdemorabitmq.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
